a82af4e7b9edd38d5b887f94b91e09516b73e9ae
Mamta Kurdia <mamtakurdia808@gmail.com>
2026-06-24 18:04:19 +0530

    Hosting

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,13 @@ const templateRoutes = require("./routes/templates.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://resume-builder-nu-eight-48.vercel.app"
+    ],
+    credentials: true
+}));
 
 app.use(express.json());
 
