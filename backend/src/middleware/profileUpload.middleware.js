c5cf141b576c5a37e7f25d2b8281f5ad1f7131fb
Mamta Kurdia <mamtakurdia808@gmail.com>
2026-06-26 16:54:48 +0530

    Upload profile photo in clodinary

--- backend/src/middleware/profileUpload.middleware.js
+++ backend/src/middleware/profileUpload.middleware.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
+const cloudinary = require("../config/clodinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
